3d2f04827bc6d196557bea2de37de3e5626215f2
BT_1N3_06 <example@bths.edu>
2024-01-04 10:52:54 -0500

    Work done on spritePanel, but layout not working.

--- src/UI.java
+++ src/UI.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.InputMismatchException;
@@ -10,6 +11,9 @@ public class UI implements ActionListener {
     private final JFrame WINDOW;
     private JButton startGame;
     private JButton closeGame;
+    private JPanel buttonPanel;
+    private JPanel spritePanel;
+    private JPanel startPanel;
     public UI () {
         WINDOW = new JFrame();
         WINDOW.setSize(900, 800);
@@ -17,8 +21,24 @@ public class UI implements ActionListener {
         WINDOW.setVisible(true);
         WINDOW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        buttonPanel = new JPanel();
+        spritePanel = new JPanel();
+        startPanel = new JPanel();
+
         startGame = new JButton();
+        formatButton(startGame);
         closeGame = new JButton();
+        formatButton(closeGame);
+
+        spritePanel.setBounds(0, 0, 900, 650);
+        spritePanel.setLayout(new BorderLayout());
+        spritePanel.setSize(900, 800);
+        spritePanel.setLayout(new GridLayout());
+        spritePanel.setVisible(true);
+        spritePanel.add(startGame);
+        spritePanel.add(closeGame);
+
+        WINDOW.add(spritePanel);
     }
 
     public void actionPerformed(ActionEvent ae) {
@@ -43,5 +63,10 @@ public class UI implements ActionListener {
     }
     public void endGame() {
         throw new UnsupportedOperationException();
+    }
+
+    private void formatButton(JButton button) {
+        button.setSize(225,150);
+        button.addActionListener(this);
     }
 }
